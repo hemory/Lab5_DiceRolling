d9721cf87cea8d38bab5153a503f4283ce8a013f
hemory <hphife@yahoo.com>
2016-09-16 21:54:20 -0400

    Add files via upload

---
+++ Lab 5/Lab 5/DiceRollerApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class DiceRollerApp
+    {
+        public void diceRoll(int d1, int d2) //To make a class you need "public" "void" and then name your variable.
+            //Then make a new variables. With "void" you dont need a return. 
+        {
+            if (d1 == 2 && d2 == 2) //your parameters
+            {
+                Console.WriteLine("Snake Eyes!"); // code to execute
+            }
+            else if (d1 == 4 && d2 == 4)
+            {
+                Console.WriteLine("Box Cars!");
+            }
+            else if (d1 == 6 && d2 == 6)
+            {
+                {
+                    Console.WriteLine("Craps!");
+                }
+            }
+
+        }
+    }
+}
---
+++ Lab 5/Lab 5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus Casino. Roll the Dice y/n?");
+
+            string answer = Console.ReadLine();
+            while (answer == "y")
+            {
+              
+                Random rnd = new Random();
+                int randomNumber = rnd.Next(1, 6);
+                int anotherRandomNumber = rnd.Next(1, 7);
+                
+                Console.WriteLine(randomNumber);
+                Console.WriteLine(anotherRandomNumber);
+                
+
+                DiceRollerApp specialRolls = new DiceRollerApp(); //DiceRollerApp is calling the class. specialRolls is 
+                //new variable assignment. = new DiceRollerApp(); is new instance.
+                
+                specialRolls.diceRoll(randomNumber, anotherRandomNumber); //specialRolls is new instance of the class. 
+                //(.diceRoll(randomNumber, anotherRandomNumber);) is calling the method
+                Console.WriteLine("Roll again? y/n");
+                answer = Console.ReadLine(); //this line takes in the user input, without loop will repeat forever.
+            }
+            
+        }
+    }
+}
+
+
+
